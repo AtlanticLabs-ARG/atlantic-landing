e9676ab5814cfae914ca3f1c85e686119e02d8bf
Emanuel Maldonado Tuero <emanuel.maldonado@atlanticlabs.com.ar>
2024-11-02 02:07:56 +0100

    feat: initial project structure

---
+++ app/components/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div>
+      <h1 className="text-8xl font-light">Raleway Light</h1>
+      <h1 className="text-8xl">Raleway Regular</h1>
+      <h1 className="text-8xl font-bold">Raleway Bold</h1>
+    </div>
+  );
+}
