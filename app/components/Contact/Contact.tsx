d17e278b7ccf06600e4a47d1b5050761e1737736
Lucia Chinni <lucia.chinni@atlanticlabs.com.ar>
2024-11-06 01:32:49 -0300

    Feat: Contact & Footer components

--- app/components/Contact/Contact.tsx
+++ app/components/Contact/Contact.tsx
@@ -1,8 +1,65 @@
+import vectorLocation from "@/assets/images/contact/vector-location.svg";
+import vectorPhone from "@/assets/images/contact/vector-phone.svg";
+import vectorEmail from "@/assets/images/contact/vector-email.svg";
+import logoInstagram from "@/assets/images/contact/logo-instagram.svg";
+import logoLinkedin from "@/assets/images/contact/logo-linkedin.svg";
+import logoFacebook from "@/assets/images/contact/logo-facebook.svg";
+
+
 export default function Contact() {
     return (
-      <section className="w-screen h-screen">
-        <div className="w-full h-full flex justify-center items-center">
-          <h1 className="text-6xl">CONTACT</h1>
+      <section className="w-screen h-full flex flex-col items-center gap-y-8" id="Contact">
+        <div className="w-full flex flex-row items-center justify-center gap-12 mb-4 p-4">
+          <div className="w-1/4 left-partner-line text-white" />
+            <h3 className="text-xl text-[#EEE4F7] text-center font-bold">
+              Contactanos
+            </h3>
+          <div className="w-1/4 right-partner-line" />
+        </div>
+        <div className="flex flex-row items-center justify-center gap-x-20">
+          <div className="flex items-center bg-[#D9D9D91A] text-white border border-[#4DA3E1] p-4 gap-4">
+            <img
+              src={vectorEmail.src}
+              className="w-6 h-6 text-white"
+              alt="vectorEmail"
+            />
+            <span className="roboto-flex text-2xl font-thin tracking-wide">info@atlanticlabs.com.ar</span>
+          </div>
+          <div className="flex items-center bg-[#D9D9D91A] text-white border border-[#4DA3E1] p-4 gap-4">
+            <img
+              src={vectorPhone.src}
+              className="w-6 h-6 text-white"
+              alt="vectorPhone"
+            />
+            <span className="roboto-flex text-2xl font-thin tracking-wide">(0223) 15 5253659</span>
+          </div>
+          <div className="flex items-center bg-[#D9D9D91A] text-white border border-[#4DA3E1] p-4 gap-4">
+            <img
+              src={vectorLocation.src}
+              className="w-6 h-6 text-white"
+              alt="vectorLocation"
+              />
+            <span className="roboto-flex text-2xl font-thin tracking-wide">Mar del Plata, Argentina</span>
+          </div>
+        </div>
+        <div className="w-full flex flex-row items-center justify-center gap-6 mb-4 p-12">
+          <div className="w-1/4 left-partner-line text-white" />
+          <img
+              src={logoInstagram.src}
+              className="w-8 h-8 text-white"
+              alt="logoInstagram"
+              />
+            <img
+              src={logoLinkedin.src}
+              className="w-8 h-8 text-white"
+              alt="logoLinkedin"
+              />
+          <img
+              src={logoFacebook.src}
+              className="w-8 h-8 text-white"
+              alt="logoFacebook"
+              />
+          <div className="w-1/4 right-partner-line" />
         </div>
       </section>
     );
--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,9 +1,45 @@
+import vectorFooter from "@/assets/images/footer/vector-bg.svg";
+import whiteLogoAtlantic from "@/assets/images/footer/white-logo-atlantic.svg";
+
 export default function Footer() {
     return (
-      <section className="w-screen h-screen">
-        <div className="w-full h-full flex justify-center items-center">
-          <h1 className="text-6xl">FOOTER</h1>
+      <section className="w-screen h-screen flex flex-row justify-center items-center relative">
+        <img
+        src={whiteLogoAtlantic.src}
+        className="object-contain absolute bottom-5 left-5"
+        alt="vectorBackground"
+        />
+
+        <div className="w-1/4 h-full flex flex-col items-start justify-end pl-20 gap-4 text-left pb-20">
+          <h3 className="text-xl font-light">
+            Sobre Nosotros
+          </h3>
+          <h3 className="text-xl font-light">
+            Nuestros Valores
+          </h3>
+          <h3 className="text-xl font-light">
+            Partners
+          </h3>
+          <h3 className="text-xl font-light">
+            Último proyecto
+          </h3>
+          <h3 className="text-xl font-light">
+            Contacto
+          </h3>
+          <h3 className="text-xl font-light">
+            Términos y condiciones
+          </h3>
         </div>
+        <div className="w-1/4 h-full flex flex-col items-end justify-end pr-20 pb-20">
+          <h3 className="text-xl font-light justify-end">
+          © 2024 Atlantic Labs
+          </h3>
+        </div>
+        <img
+        src={vectorFooter.src}
+        className="h-full object-contain absolute top-0 right-0"
+        alt="vectorBackground"
+        />
       </section>
     );
   }
